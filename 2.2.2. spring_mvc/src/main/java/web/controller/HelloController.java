02c3b92aadefb6a2dce683d3a12e4f70b0edb4d2
Kostya Petrov <KostyaPetrov1986@gmail.com>
2020-10-07 12:18:46 +0400

    исправил @Autowired поле приватное тип поля интерфейс

--- 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
@@ -1,10 +1,10 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
@@ -13,9 +13,8 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-    CarServiceImpl service;
+    private final CarService service;
 
-    @Autowired
     public HelloController(CarServiceImpl service) {
         this.service = service;
     }
